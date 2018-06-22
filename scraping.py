75d12d5b2c54d3936edef0fa2afa6069927b6f01
ryhord <ryhord@users.noreply.github.com>
2018-06-21 22:51:59 -0400

    went back to using beautiful soup. added funtions to take in url to parse and better printing

--- scraping.py
+++ scraping.py
@@ -2,14 +2,15 @@ from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
+""" 
 import scrapy
 
 class NCSSpider(scrapy.Spider):
-    """A Scrapy spider for No Clean Singing"""
+    A Scrapy spider for No Clean Singing
     name = 'NCS'
 
     start_urls = [
-        'https://www.nocleansinging.com/',
+        'http://www.metalinjection.net/lists/our-picks-for-the-top-metal-albums-of-2018-so-far/2',
     ]
 
     def parse(self, response):
@@ -21,11 +22,9 @@ class Page(scrapy.Item):
 
 """  
 def simple_get(url):
-    
-    Attempts to get the content at `url` by making an HTTP GET request.
+    """    Attempts to get the content at `url` by making an HTTP GET request.
     If the content-type of response is some kind of HTML/XML, return the
-    text content, otherwise return None
-    
+    text content, otherwise return None"""
     try:
         with closing(get(url, stream=True)) as resp:
             if is_good_response(resp):
@@ -37,31 +36,33 @@ def simple_get(url):
         return None
 
 def is_good_response(resp):
-    
-    Returns true if the response seems to be HTML, false otherwise
-    
+    """
+    Returns True if the response seems to be HTML, False otherwise.
+    """
     content_type = resp.headers['Content-Type'].lower()
     return (resp.status_code == 200
             and content_type is not None
             and content_type.find('html') > -1)
 
 def log_error(e):
-    
-    It is always a good idea to log errors.
+    """    It is always a good idea to log errors.
     This function just prints them, but you can make it do anything
-    
+    """
     print(e)
 
 
-def get_selections(url):
+def get_selections(url, selection):
     raw_html = simple_get(url)
     html = BeautifulSoup(raw_html, 'html.parser')
-    songs = html.select('.article-detial h3')
+    songs = html.select(selection)
+    """
     songs = list(songs)
+   
     songs_as_strings = []
     for song in songs:
         songs_as_strings.append(song.string.extract())
-    return songs_as_strings
+    """
+    return songs
 
 def put_to_file(items):
     file = open("list.txt", "w")
@@ -74,9 +75,27 @@ def put_to_file(items):
 #
 #
 
+def get_names(url, selector):
+    """
+    Downloads the page where the list of music is found
+    and returns a list of strings, one per n
+    """
+    response = simple_get(url)
+
+    if response is not None:
+        html = BeautifulSoup(response, 'html.parser')
+        names = set()
+        for li in html.select(selector):
+            for name in li.text.split('\n'):
+                if len(name) > 0:
+                    names.add(name.strip())
+        return list(names)
+
+    # Raise an exception if we failed to get any data from the url
+    raise Exception('Error retrieving contents at {}'.format(url))
+
 url = input('enter the url you want to get: ')
-list_of_songs = get_selections(url)
+selection = input("What's the selector of the content you want?")
+list_of_songs = get_names(url, selection)
 for song in list_of_songs:
     print(song)
-    
-"""
